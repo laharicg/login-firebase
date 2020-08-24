e1adf1e0d4dae80204031326fa58d0245e24aab2
lahari govada <govada.lahari@gmail.com>
2020-08-25 01:59:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
-import React, {Component} from 'react';
-import fire from './config/Fire';
-import Login from './Login';
-import Home from './Home';
+import React, { Component } from "react";
+import fire from "./config/Fire";
+import Login from "./Login";
+import Home from "./Home";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      user:'',
-    }
+    this.state = {
+      user: "",
+    };
   }
 
-  componentDidMount(){
-   this.authListener();
+  componentDidMount() {
+    this.authListener();
   }
 
-
-  authListener(){
+  authListener() {
     fire.auth().onAuthStateChanged((user) => {
-     console.log(user);
-      if(user) {
-        this.setState({user});
-       // localStorage.setItem('user',user.uid);
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        // localStorage.setItem('user',user.uid);
       } else {
-        this.setState({user:null});
+        this.setState({ user: null });
         // localStorage.setItem('user');
       }
     });
   }
-render() {
-  return (
-    <div className="App">
-      {this.state.user ? (<Home />) : (<Login />)}
-    </div>
-  );
+  render() {
+    return <div className="App">{this.state.user ? <Home /> : <Login />}</div>;
+  }
 }
-
-}
-  
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import fire from "./config/Fire";
-//import fire from './config/Fire';
+import io from "socket.io-client";
+const ENDPOINT = "http://localhost:8000/";
+
+//Creating socket to connect to SERVER
+var socket = io.connect(ENDPOINT);
 
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      button1: false,
+      button2: false,
+    };
+    this.click1 = this.click1.bind(this);
+    this.click2 = this.click2.bind(this);
+    this.setButton1 = this.setButton1.bind(this);
+    this.setButton2 = this.setButton2.bind(this);
     this.logout = this.logout.bind(this);
   }
 
@@ -12,12 +24,70 @@ class Home extends Component {
     fire.auth().signOut();
   }
 
+  click1() {
+    console.log("Clicked button 1");
+    socket.emit("connect1", {
+      clicked: true,
+    });
+  }
+
+  click2() {
+    socket.emit("connect2", {
+      clicked: true,
+    });
+    console.log("Clicked Button 2");
+  }
+
+  setButton1() {
+    this.setState((buttonState) => ({
+      button1: !this.state.button1,
+    }));
+  }
+
+  setButton2() {
+    this.setState((buttonState) => ({
+      button2: !this.state.button2,
+    }));
+  }
+
+  componentWillMount() {
+    console.log("Component WILL MOUNT!");
+    socket.on("connect1", (data) => {
+      if (data.clicked === true) {
+        this.setButton1();
+        console.log(data, "Button 1 clicked From Server");
+      }
+    });
+
+    socket.on("connect2", (data) => {
+      if (data.clicked === true) {
+        this.setButton2();
+        console.log(data, "Button 2 clicked From Server");
+      }
+    });
+  }
+
   render() {
     return (
       <div className="col-md-6">
         <h1> You are Home!</h1>
-        <button className="btn btn-warning">Button 1</button> &nbsp;
-        <button className="btn btn-info">Button 2</button>
+        <button
+          className="btn btn-warning"
+          onClick={this.click1}
+          id="button1"
+          disabled={this.state.button1}
+        >
+          Button 1
+        </button>{" "}
+        &nbsp;
+        <button
+          className="btn btn-warning"
+          onClick={this.click2}
+          id="button2"
+          disabled={this.state.button2}
+        >
+          Button 2
+        </button>
         <br></br>
         <br></br>
         <button onClick={this.logout} className="btn btn-danger">
